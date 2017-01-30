3e0bcdbee8ed4b53827faa01b3afb0f55534cf0c
Kevin Ngo <Kevin.ngo0407@gmail.com>
2017-01-30 17:20:12 -0500

    Init

---
+++ app/src/main/java/com/example/kevin/steamworksscout/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.kevin.steamworksscout;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
